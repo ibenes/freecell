530a02aab1d1997b220a512cd3a014847efe69cf
Benes Karel <ibenes@fit.vutbr.cz>
2022-09-04 11:06:41 +0200

    Introduce tests of free cell

--- test.cc
+++ test.cc
@@ -2,11 +2,19 @@
 #include "catch.hpp"
 
 #include "card.h"
+#include "card-storage.h"
+
 #include <sstream>
 
 std::string cardRepresentation(const Card &card) {
 	std::stringstream ss;
 	ss << card;
+	return ss.str();
+}
+
+std::string freeCellRepresentation(const FreeCell &fc) {
+	std::stringstream ss;
+	ss << fc;
 	return ss.str();
 }
 
@@ -28,4 +36,22 @@ TEST_CASE( "Card construction and printing tests", "[card]" ) {
 	REQUIRE(cardRepresentation({Color::Diamond, 7}) == "7d");
 	REQUIRE(cardRepresentation({Color::Club, 7}) == "7c");
 	REQUIRE(cardRepresentation({Color::Spade, 7}) == "7s");
+}
+
+TEST_CASE( "FreeCell operations", "[freecell]" ) {
+    FreeCell free_cell;
+	REQUIRE(freeCellRepresentation(free_cell) == "_");
+
+	REQUIRE(free_cell.topCard() == nullptr);
+	REQUIRE(free_cell.getCard() == nullptr);
+
+	auto c = std::make_unique<Card>(Color::Heart, 1);
+	REQUIRE(free_cell.canAccept(*c));
+	REQUIRE(free_cell.acceptCard(std::move(c)));
+
+	REQUIRE(freeCellRepresentation(free_cell) == "1h");
+	REQUIRE(cardRepresentation(*free_cell.topCard()) == "1h");
+	REQUIRE(cardRepresentation(*free_cell.getCard()) == "1h");
+
+	REQUIRE(freeCellRepresentation(free_cell) == "_");
 }
