2f26f0eaff590ba78e69370602a48d38e9350ebe
Benes Karel <ibenes@fit.vutbr.cz>
2022-09-19 16:13:48 +0200

    Introduce evaluation infrastructure

--- fc-sui.cc
+++ fc-sui.cc
@@ -4,8 +4,51 @@
 #include "search-strategies.h"
 
 #include <cassert>
+#include <chrono>
 #include <iostream>
 #include <memory>
+
+
+struct StrategyEvaluation {
+    unsigned long nb_solved;
+    unsigned long nb_failed;
+    unsigned long total_solution_length;
+    std::chrono::microseconds time_taken;
+};
+
+std::ostream& operator<< (std::ostream& os, const StrategyEvaluation &report) {
+    os << "Solved " << report.nb_solved << " / " << report.nb_solved + report.nb_failed <<
+        " [ " << 100.0*report.nb_solved / (report.nb_solved + report.nb_failed) << " % ]. " <<
+        "Avg solution length " << 1.0 * report.total_solution_length / report.nb_solved << " steps, "
+        "Avg time taken: " << (report.time_taken / report.nb_solved).count() << " us \n";
+
+    return os;
+}
+
+
+void eval_strategy(
+        std::unique_ptr<SearchStrategyItf> &search_strategy,
+        const SearchState &init_state,
+        StrategyEvaluation *report
+    ) {
+
+    auto t0 = std::chrono::steady_clock::now();
+	auto solution = search_strategy->solve(init_state);
+    auto t1 = std::chrono::steady_clock::now();
+
+
+	SearchState in_progress(init_state);
+	for (const auto & action : solution)
+		in_progress = action.execute(in_progress);
+
+    if (in_progress.isFinal()) {
+        report->nb_solved++;
+        report->total_solution_length += solution.size();
+        report->time_taken += std::chrono::duration_cast<decltype(report->time_taken)>(t1 - t0);
+    } else {
+        report->nb_failed++;
+    }
+}
 
 
 int main() {
@@ -13,35 +56,21 @@ int main() {
     std::default_random_engine rng(1);
 
 	std::unique_ptr<SearchStrategyItf> search_strategy = std::make_unique<DummySearch>();
+    StrategyEvaluation evaluation_record{};
 
-    GameState gs{};
-    initializeGameState(&gs, rng);
-    std::cout << gs;
-	std::cout << "\n";
+    for (int i = 0; i < 42; ++i) {
+        GameState gs{};
+        initializeGameState(&gs, rng);
 
-	std::optional<std::pair<CardStorage *, WorkStack * >> irr_move;
-	while((irr_move = findIrreversibleMove(&gs, rng)), irr_move.has_value()) {
-		forceMove(irr_move->first, irr_move->second);
-		std::cout << gs;
-		std::cout << "\n";
-		break;
-	}
+        std::optional<std::pair<CardStorage *, WorkStack * >> irr_move;
+        while((irr_move = findIrreversibleMove(&gs, rng)), irr_move.has_value()) {
+            forceMove(irr_move->first, irr_move->second);
+            break;
+        }
+        SearchState init_state(gs);
 
-	SearchState init_state(gs);
-	std::cout << init_state;
-	std::cout << "State finality: " << init_state.isFinal() << "\n";
+        eval_strategy(search_strategy, init_state, &evaluation_record);
+    }
 
-	auto solution = search_strategy->solve(init_state);
-
-	SearchState in_progress(init_state);
-	for (const auto & action : solution) {
-		std::cout << "Execution action " << action << "\n";
-		in_progress = action.execute(in_progress);
-		std::cout << in_progress;
-		std::cout << "State finality: " << in_progress.isFinal() << "\n";
-	}
-
-	std::cout << "\nFor comparison, the initial state was:\n";
-	std::cout << init_state;
-	std::cout << "State finality: " << init_state.isFinal() << "\n";
+    std::cout << evaluation_record;
 }
--- search-strategies.cc
+++ search-strategies.cc
@@ -1,8 +1,13 @@
 #include "search-strategies.h"
 
+#include <chrono>
+#include <thread>
+
 std::vector<SearchAction> DummySearch::solve(const SearchState &init_state) {
 	std::vector<SearchAction> solution;
 	solution.push_back(init_state.actions()[14]);
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
 	return solution;
 }
