716394d084e5d7a14dea2aa2072a7709a156c3da
Benes Karel <ibenes@fit.vutbr.cz>
2022-09-23 20:04:50 +0200

    Make sure StrategyEvaluation is properly initialized

--- evaluation-type.h
+++ evaluation-type.h
@@ -5,6 +5,7 @@
 #include <iostream>
 
 struct StrategyEvaluation {
+	StrategyEvaluation() : nb_solved(0), nb_failed(0), total_solution_length(0), nb_states_expanded(0), time_taken(0) {}
     unsigned long nb_solved;
     unsigned long nb_failed;
     unsigned long total_solution_length;
