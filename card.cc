e65c458066d83e5758e3db9c929d7bdeffe4a811
guritchi <80781072+guritchi@users.noreply.github.com>
2023-10-26 23:35:11 +0200

    console pretty print

--- card.cc
+++ card.cc
@@ -2,11 +2,24 @@
 
 #include <cassert>
 
+#ifdef CONSOLE_COLORS
+static constexpr auto ansi_reset = "\033[0m";
+static constexpr auto ansi_bold = "\033[1m";
+static constexpr auto ansi_red = "\033[1;31m";
+#endif
+
 const std::map<Color, std::string> color_map{
-	{Color::Heart, "h"},
-	{Color::Diamond, "d"},
-	{Color::Club, "c"},
-	{Color::Spade, "s"},
+#ifdef CONSOLE_COLORS
+    {Color::Heart, "\u2665"},
+    {Color::Diamond, "\u2666"},
+    {Color::Club, "\u2663"},
+    {Color::Spade, "\u2660"},
+#else
+    {Color::Heart, "h"},
+    {Color::Diamond, "d"},
+    {Color::Club, "c"},
+    {Color::Spade, "s"},
+#endif
 };
 
 const std::map<Color, RenderColor> render_color_map{
@@ -28,16 +41,29 @@ Card::Card(Color col, int val) : color(col), value(val) {
 }
 
 std::ostream& operator<< (std::ostream& os, const Card & card) {
-	if (card.value <= 10) {
-		os << card.value;
+#ifdef CONSOLE_COLORS
+    if (render_color_map.at(card.color) == RenderColor::Red) {
+        os << ansi_red;
+    }
+    else {
+        os << ansi_bold;
+    }
+#endif
+    if (card.value < 10) {
+        os << " " << card.value;
+    } else if (card.value == 10) {
+        os << "10";
 	} else if (card.value == 11) {
-		os << "J";
+        os << " J";
 	} else if (card.value == 12) {
-		os << "Q";
+        os << " Q";
 	} else if (card.value == 13) {
-		os << "K";
+        os << " K";
 	}
-	os << color_map.at(card.color);
+	os << color_map.at(card.color);    
+#ifdef CONSOLE_COLORS
+    os << " " << ansi_reset;
+#endif
 	return os;
 }
 
